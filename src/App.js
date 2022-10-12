f1e831fdd0f6389fe6f6f2e49c0506d417020d3e
Amelie Pira <amelie.pira@gmail.com>
2022-10-12 20:20:03 +0100

    removed unecessary apollo client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import "antd/dist/antd.min.css";
 import { HashRouter as Router } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 import { Footer } from "./components/Footer";
 import { Navbar } from "./components/Navbar";
 import { AppRoutes } from "./AppRoutes";
 
 import "./App.css";
 
-const client = new ApolloClient({
-  uri: "http://localhost:4000/",
-  cache: new InMemoryCache(),
-});
-
 export const App = () => {
   return (
-    <ApolloProvider client={client}>
+    <>
       <Router>
         <Navbar />
         <AppRoutes />
       </Router>
       <Footer />
-    </ApolloProvider>
+    </>
   );
 };
